be31b5d897e3f34d732b73f3b2a279915ced9246
Thiele Stefan (CS/EBS1) <ths2be@bosch.com>
2019-05-13 10:05:16 +0200

    aufgeräumt

--- app.js
+++ app.js
@@ -3,32 +3,20 @@
 //requires
 var osrm_client = require('./osrm_client');
 
-// POI geojson from Berlin and Brandenburg
-// relativen pfad angeben
-var obj = require("/Users/ths2be/Desktop/BA/service/POI_20190507.json");
-
-// new array with all POI coordinates
-var features = Object.values(obj)[4];
-var formattedArray = features.map(obj => {
-    var x = [];
-    x = obj.geometry.coordinates;
-    return x;
-});
-
 // swap array entries to right geo coordinates for OSRM
-var arrayLength = formattedArray.length;
+/* var arrayLength = formattedArray.length;
 for (var i = arrayLength; i--;) {
     var tmp = formattedArray[i][0];
     formattedArray[i][0] = formattedArray[i][1];
     formattedArray[i][1] = tmp; 
-};
+}; */
 
-
-osrm_client.closestPOI([25,5]);
+var nearestPoiToA = osrm_client.closestPOI([13.454132,52.557986]);
 
 // callback: hier geht es dann weiter mit  der bvg
 function resultCallback (body) {
     console.log('BVG starts here');    
 };
 
-osrm_client.routeToPoi('13.388860,52.517037','13.397634,52.529407', resultCallback);
+//osrm_client.routeToPoi('13.388860,52.517037','13.397634,52.529407', resultCallback);
+osrm_client.routeToPoi(nearestPoiToA,'13.397634,52.529407', resultCallback);
--- osrm_client.js
+++ osrm_client.js
@@ -1,27 +1,43 @@
-//js with all functions
-
 //requires
 var geolib = require('geolib');
 
+// POI geojson from Berlin and Brandenburg
+var obj = require('./POI_20190507.json');
+
+// new array with all POI coordinates
+var features = Object.values(obj)[4];
+var formattedArray = features.map(obj => {
+    var x = [];
+    x = obj.geometry.coordinates;
+    return x;
+});
+console.log(formattedArray);
+//13.2774765, 52.5974537
+
 module.exports = {
     
-    closestPOI: function  (current){
-        var coords = [[5,5], [10.2,19.6], [60, 10]];
+    // find closest POI to start A
+    closestPOI: function  (fromA){
+        var testCoords = [[5,5], [10.2,19.6], [60, 10]];
 
-        var distFromCurrent = function(coord) {
+        // POI array
+        var pois = formattedArray;
+
+        var distFromCurrent = function(coordinates) {
             return {
-                coord: coord, dist: geolib.getDistance(current, coord)
+                coordinates: coordinates, dist: geolib.getDistance(fromA, coordinates)
             };
         }
 
-        var closest = coords.map(distFromCurrent).sort(function(a,b)  {
+        var closest = pois.map(distFromCurrent).sort(function(a,b)  {
             return a.dist - b.dist
         })[0];
 
-        console.log("Closest: " + closest.coord[0] + ", " + closest.coord[1]);
+        console.log("Closest: " + closest.coordinates[0] + ", " + closest.coordinates[1]);
         return closest;
     },
 
+    // route from A to POI
     routeToPoi: function (fromCoordinates, toCoordinates, callback){
         var baseUrl = 'http://router.project-osrm.org/';
         var urlPath = 'route/v1/driving/' + fromCoordinates + ';' + toCoordinates + '?alternatives=3';
