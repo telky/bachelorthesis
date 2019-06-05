37ac11a9abea92120c3ec6cabee61b7e1b87b48a
Thiele Stefan (CS/EBS1) <ths2be@bosch.com>
2019-06-05 13:59:24 +0200

    routing is working now

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 //requires
 var osrm_client = require('./osrm_client');
+var geojson = require('geojson');
 
 // predefined routes
 var routeA = require('./RoutesA.json');
@@ -10,24 +11,61 @@ var routeB = require('./RoutesB.json');
 // get all coordinates from A and put them into a new array
 var allRouteA = Object.values(routeA)[0];
 var aList = allRouteA.map(routeA => {
-    var x = [];
-    x = routeA.coordinates;
-    return x;
+    var a = [];
+    a = routeA.coordinates;
+    return a;
 });
 
-console.log('From A: ' + aList[0]);
+// get all destination coordinates from B and put them into a new array
+var allRouteB = Object.values(routeB)[0];
+var bList = allRouteB.map(routeB => {
+    var b = [];
+    b = routeB.coordinates;
+    return b;
+});
 
-// TODO: für jede aList[i] den nächsten POI finden
 var findPoiToA = osrm_client.closestPOI(aList[0]);
 var nearestPoiToA = findPoiToA.coordinates[0] + "," + findPoiToA.coordinates[1];
-console.log('To POI: ' + nearestPoiToA);
+var parsePoiToA = JSON.parse('[' + nearestPoiToA + ']');
+console.log(parsePoiToA);
 
-// callback: hier geht es dann weiter mit  der bvg
-function resultCallback (body) {
-    console.log('BVG starts here');    
+// routing from Start to POI
+osrm_client.routeToPoi(aList[0],parsePoiToA, resultRouteFromAToPoi);
+
+// routing from POI to Destination and parse some geojson
+function resultRouteFromAToPoi (body) {
+
+    osrm_client.routeToB(aList[0],bList[0], resultRouteFromPoiToB);
+
+    var test = Object.values(body)[1];
+
+    var steps = test.map(body => {
+        return body.legs[0].steps;
+    });
+
+    for (let i = 0; i < steps.length; i++) {
+        var element = steps[i];
+        for (let j = 0; j < element[i].length; j++) {
+            var coords = element[j];
+            console.log(coords);
+            
+        }
+        // console.log(element);       
+    }
+
+    // console.log(steps);
+    
+    // console.log(test);
+    // console.log(test.legs);
+    // console.log(document.body);
+    // geojson.parse(body);
 };
 
-// TODO: von jedem POI zum aList[i] routen
-osrm_client.routeToPoi(aList[0],nearestPoiToA, resultCallback);
+// vbb routing and parse some geojson
+function resultRouteFromPoiToB (body) {
+    console.log('Routing vom POI zu B!');
+    // console.log(body);   
 
-//osrm_client.routeToB('var_von_POI', 'var_zum_B', 'callback_was_danach_mit_der_route_getan_werden_soll');
+};
+
+// geojson mapping
--- osrm_client.js
+++ osrm_client.js
@@ -1,3 +1,5 @@
+// Bachelorthesis - Stefan Thiele @TUB
+
 //requires
 const geolib = require('geolib');
 const request = require('request');
@@ -34,36 +36,49 @@ module.exports = {
         return closest;
     },
 
-    // route from A to POI
+    // Route from A to POI
     routeToPoi: function (fromCoordinates, toCoordinates, callback){
 
         var baseUrl = 'http://localhost:5000/';
-        var urlPath = 'route/v1/driving/' + fromCoordinates + ';' + toCoordinates + '?alternatives=3';
+        var urlPath = 'route/v1/driving/' + fromCoordinates + ';' + toCoordinates + '?alternatives=3&steps=true';
         
         request(baseUrl + urlPath, { json : true }, (err, res, body) => {
 
             if(err) {
-                return console.log(err);
+                return console.log('POI ERROR:', err);
             }
 
             callback(body);
-            console.log(body);
+            //console.log(res);
+
         });
     },
 
-    // TODO route from POI to B, VBB endpoint
+    // VBB API: routing from POI to B
     routeToB: function (fromCoordinates, toCoordinates, callback) {
         
-        var baseUrl = '';
-        var urlPath = '';
-        request(baseUrl + urlPath, { json : true}, (err, res, body) => {
+        var baseUrl = 'http://demo.hafas.de/openapi/vbb-proxy/trip?';
+        var fromLat = 'originCoordLat=';
+        var fromLatCoords = fromCoordinates[1];
+        var fromLong = 'originCoordLong=';
+        var fromLongCoords = fromCoordinates[0];
+        var destLat = 'destCoordLat=';
+        var destLatCoords = toCoordinates[1];
+        var destLong = 'destCoordLong=';
+        var destLongCoords = toCoordinates[0];
+        var accessId = ';format=json;accessId=bosch-Thiele-4035-900f-29403c078264';
+        request(baseUrl + fromLat + fromLatCoords + ';' + fromLong + fromLongCoords + ';' + destLat + destLatCoords + ';' + destLong + destLongCoords + accessId, { json : true}, (err, res, body) => {
 
             if(err) {
-                return console.log(err);
+                return console.log('VBB ERROR:',err);
             }
-
+            
             callback(body);
-            //console.log(body);
+            //console.log(res);
+
         });
     }
 };
+
+// bsp trip
+//http://demo.hafas.de/openapi/vbb-proxy/trip?originCoordLat=52.5638554;originCoordLong=13.4412406;destCoordLat=52.506191;destCoordLong=13.330536;format=json;accessId=bosch-Thiele-4035-900f-29403c078264
